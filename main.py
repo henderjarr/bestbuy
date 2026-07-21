333d521322c4073da6919946d02d90d24f517c7f
jarrod henderson <jarrodhenderson@jarrods-MacBook-Pro.local>
2026-07-21 07:52:42 +0200

    clean up code

--- main.py
+++ main.py
@@ -1,41 +1,17 @@
 from products import Product
 from store import Store
 
-# bose = Product("Bose QuietComfort Earbuds", price=250, quantity=500)
-# mac = Product("MacBook Air M2", price=1450, quantity=100)
-
-# print(bose.buy(50))
-# print(mac.buy(100))
-# print(mac.is_active())
-
-# bose.show()
-# mac.show()
-
-# bose.set_quantity(1000)
-# bose.show()
-
-# product_list = [Product("MacBook Air M2", price=1450, quantity=100),
-#                 Product("Bose QuietComfort Earbuds",
-#                         price=250, quantity=500),
-#                 Product("Google Pixel 7", price=500, quantity=250),
-#                 ]
-
-# best_buy = Store(product_list)
-# products = best_buy.get_all_products()
-# print(f" quantity: {best_buy.get_total_quantity()}")
-# print(f"total price: {best_buy.order([(products[0], 1), (products[1], 2)])}")
-
-
 # setup initial stock of inventory
 product_list = [Product("MacBook Air M2", price=1450, quantity=100),
                 Product("Bose QuietComfort Earbuds", price=250, quantity=500),
                 Product("Google Pixel 7", price=500, quantity=250)
                 ]
+# create a store with the initial stock
 best_buy = Store(product_list)
 
 
 def start(store):
-
+    """starts the store menu"""
     while True:
         print("Store Menu\n----------\n1. List all products in store\n2. Show total amount in store\n3. Make an order\n4. Exit")
 
